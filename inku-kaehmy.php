0827785d1b15a19ef39b514dd2def0d1af39c698
vahvero <otso.brummer@aalto.fi>
2018-06-30 21:18:45 +0300

    wp plugin

---
+++ inku-kaehmy.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name:       Inku Kaehmy
+ * Description:       Plugin for handling kaehmy.
+ * Version:           0.0.1
+ * Author:            Best coders of code
+ * License:           WTFPL
+ * Text Domain:       inku-kaehmy
+ * Domain Path:				/languages
+ */
+
+class Inku_Kaehmy_Plugin {
+  /*
+   * Initialization method
+   */
+  public function __construct() {
+    // init code
+  }
+}
+
+$inku_kaehmy = new Inku_Kaehmy_Plugin();
