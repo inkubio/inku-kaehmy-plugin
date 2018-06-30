962f605a8adbd2cb079c8cd7f2bb01c502973be7
ykoski <ymkoski@gmail.com>
2018-06-30 23:30:08 +0300

    Add functions to remove comments and grabbings by id.

--- functions.php
+++ functions.php
@@ -48,4 +48,21 @@ function test() {
     return get_all_grabbings();
 }
 
+
+function get_all_tags() {
+    global $wpdb;
+
+    $query = "SELECT * FROM inku_kaehmy_tag;";
+
+    $res = $wpdb->get_results($query, ARRAY_A);
+
+    if($res){
+        return $res;
+    }
+    else{
+        return http_response_code(204);
+    }
+
+}
+
 ?>
