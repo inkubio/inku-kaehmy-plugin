407a54b7e968db0e470911feb856d1578f9829ce
ykoski <ymkoski@gmail.com>
2018-06-30 21:02:26 +0300

    Fix db-init.

--- db-init.sql
+++ db-init.sql
@@ -1,51 +1,66 @@
-DROP TABLE IF EXISTS inku-kaehmy_grabbing
-DROP TABLE IF EXISTS inku-kaehmy_comment
-DROP TABLE IF EXISTS inku-kaehmy_has_comment
-DROP TABLE IF EXISTS inku-kaehmy_tag
+DROP TABLE IF EXISTS inku_kaehmy_has_comment;
+DROP TABLE IF EXISTS inku_kaehmy_is_tagged;
+DROP TABLE IF EXISTS inku_kaehmy_tag;
+DROP TABLE IF EXISTS inku_kaehmy_grabbing;
+DROP TABLE IF EXISTS inku_kaehmy_comment;
 
-CREATE TABLE inku-kaehmy_grabbing {
-    ID INT NOT NULL,
+CREATE TABLE inku_kaehmy_grabbing (
+    ID INT PRIMARY KEY,
     userID INT NOT NULL,
     is_hallitus TINYINT NOT NULL,
     grabbing_text VARCHAR(65000) NOT NULL,
     grabbing_title VARCHAR(100) NOT NULL,
     time_stamp DATETIME NOT NULL,
-    grabbing_batch VARCHAR(100),
-    PRIMARY KEY (ID)
-}
+    grabbing_batch VARCHAR(100)
+);
 
-CREATE TABLE inku-kaehmy_comment {
-    ID INT NOT NULL,
+CREATE TABLE inku_kaehmy_comment (
+    ID INT PRIMARY KEY,
     userID INT NOT NULL,
     comment_text VARCHAR(65000) NOT NULL,
     time_stamp DATETIME NOT NULL,
-    depth INT NOT NULL,
-    PRIMARY KEY (ID)
-}
+    depth INT NOT NULL
+);
 
-CREATE TABLE inku-kaehmy_has_comment {
+CREATE TABLE inku_kaehmy_has_comment (
     comment_ID INT NOT NULL,
     parent_grabbing_ID INT,
-    parent_comment_ID INT,
-    FOREIGN KEY (comment_ID),
-    REFERENCES inku-kaehmy_comment(ID),
-    FOREIGN KEY (parent_grabbing_ID),
-    REFERENCES inku-kaehmy_grabbing(ID)
-    FOREIGN KEY (parent_comment_ID),
-    REFERENCES inku-kaehmy_comment(ID)
-}
+    parent_comment_ID INT
+);
 
-CREATE TABLE inku-kaehmy_tag {
+CREATE TABLE inku_kaehmy_tag (
     ID INT NOT NULL,
     tag_name_fi VARCHAR(100) NOT NULL,
-    tag_name_en VARCHAR(100)
-}
+    tag_name_en VARCHAR(100),
+    PRIMARY KEY (ID)
+);
 
-CREATE TABLE is_tagged {
+CREATE TABLE inku_kaehmy_is_tagged (
     grabbing_ID INT NOT NULL,
-    tag_ID INT NOT NULL,
-    FOREIGN KEY (grabbing_ID),
-    REFERENCES inku-kaehmy_grabbing(ID),
-    FOREIGN KEY (tag_ID),
-    REFERENCES inku-kaehmy_tag(ID)
-}
+    tag_ID INT NOT NULL
+);
+
+ALTER TABLE inku_kaehmy_is_tagged ADD CONSTRAINT istagged_grabbing_id_fk
+FOREIGN KEY (grabbing_ID) REFERENCES inku_kaehmy_grabbing (ID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE inku_kaehmy_is_tagged ADD CONSTRAINT istagged_tag_id_fk
+FOREIGN KEY (tag_ID) REFERENCES inku_kaehmy_tag (ID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE inku_kaehmy_has_comment ADD CONSTRAINT hc_comment_id_fk
+FOREIGN KEY (comment_ID) REFERENCES inku_kaehmy_comment (ID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE inku_kaehmy_has_comment ADD CONSTRAINT hc_comment_parent_g_fk
+FOREIGN KEY (parent_grabbing_ID) REFERENCES inku_kaehmy_grabbing (ID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE inku_kaehmy_has_comment ADD CONSTRAINT hc_comment_parent_c_fk
+FOREIGN KEY (parent_comment_ID) REFERENCES inku_kaehmy_comment (ID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
