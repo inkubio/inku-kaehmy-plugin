a85595d9c10dd76610b0e3aa646f3f76f8610e58
Jonesus <joonas.palosuo@gmail.com>
2018-07-01 22:28:54 +0300

    Fix CORS issue

--- inku-kaehmy.php
+++ inku-kaehmy.php
@@ -15,6 +15,19 @@ class Inku_Kaehmy_Plugin {
   public function __construct() {
     add_action('admin_menu', array($this, 'register_admin_menu'));
     add_action('rest_api_init', array($this, 'register_routes'));
+
+    /* CORS headers */
+    add_action( 'rest_api_init', function() {
+	    remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+	      add_filter( 'rest_pre_serve_request', function( $value ) {
+		      header( 'Access-Control-Allow-Origin: *' );
+		      header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
+		      header( 'Access-Control-Allow-Credentials: true' );
+
+		      return $value;		
+      	});
+      }, 15
+    );
   }
 
   // Add action to get tags from db.
